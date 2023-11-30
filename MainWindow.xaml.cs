2d3f213da03fb94e7a7b38cfce5ba8367faac414
Olga Bachurina <bachuri12@ukr.net>
2023-11-30 11:43:15 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApphomework27.Models;
+using WpfApphomework27.Servises;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WpfApphomework27
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BindingList<TodoModel>_todoDataList;
+        private FileIOServise _fileIOServise;
+        private readonly string PATH = $"{Environment.CurrentDirectory}\\todoDataList.json";
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        
+                private void _todoDataList_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            if (e.ListChangedType==ListChangedType.ItemAdded || e.ListChangedType == ListChangedType.ItemDeleted|| e.ListChangedType == ListChangedType.ItemChanged)
+            {
+                try
+                {
+                    _fileIOServise.SaveDate(sender);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    Close();
+                }
+            }
+        }
+
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            _fileIOServise = new FileIOServise(PATH);
+
+            /* _todoDataList = new BindingList<TodoModel>()*/
+            try
+            {
+                _todoDataList = _fileIOServise.LoadData();
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show (ex.Message);
+                Close();
+            }
+            dgTodolist.ItemsSource = _todoDataList;
+            _todoDataList.ListChanged += _todoDataList_ListChanged;
+        }
+       
+    }
+
+}
---
+++ Models/TodoModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace WpfApphomework27.Models
+{
+    public class TodoModel:INotifyPropertyChanged
+    {
+        
+        private bool _isDone;
+        private string _text;
+        public SolidColorBrush Color { get; set; }
+        public DateTime Creationdate { get; set; } = DateTime.Now;
+
+        public bool IsDone
+        {
+            get { return _isDone; }
+            set
+            {
+                if (_isDone == value)
+                    return;
+                _isDone = value;
+                OnPropertyChanged("IsDone");
+            }
+        }
+      
+      public string Text
+        {
+            get { return _text; }
+            set
+            {
+                if (_text == value)
+                    return;
+                _text = value;
+                OnPropertyChanged("Text");
+            }
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyname="")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyname));
+            
+        }
+    }
+}
